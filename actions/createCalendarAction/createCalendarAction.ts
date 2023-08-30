babe20e7715344e730c4752a5767eea0194d061f
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2023-08-30 23:54:57 +0200

    UI Improvements

--- actions/createCalendarAction/createCalendarAction.ts
+++ actions/createCalendarAction/createCalendarAction.ts
@@ -7,5 +7,7 @@ import { createCalendarSchema } from '@/schemas/createCalendarSchema';
 export const createCalendarAction = createServerAction(
   createCalendarSchema,
   createCalendar,
-  '/[calendarId]',
+  {
+    tag: 'calendars',
+  },
 );
