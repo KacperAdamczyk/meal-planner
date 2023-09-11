457a019bf1e144e692b97b4c0482104f0baf2e25
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2023-09-11 23:27:32 +0200

    Migrate to bun test

--- db/actions/helpers/getMonthBoundary/getMonthBoundary.test.ts
+++ db/actions/helpers/getMonthBoundary/getMonthBoundary.test.ts
@@ -1,29 +1,16 @@
 import { getMonthBoundary } from '@/db/actions/helpers/getMonthBoundary';
-import { describe, expect, test } from 'vitest';
+// import { describe, expect, test } from 'vitest';
+import { describe, test, expect } from 'bun:test';
 
 describe('getMonthBoundary', () => {
-  test.each`
-    month     | year    | from            | to
-    ${9 - 1}  | ${2023} | ${'2023-08-27'} | ${'2023-09-30'}
-    ${8 - 1}  | ${2023} | ${'2023-07-30'} | ${'2023-09-02'}
-    ${10 - 1} | ${2023} | ${'2023-10-01'} | ${'2023-11-04'}
-  `(
-    'calculates boundary dates for $month + 1/$year',
-    ({
-      month,
-      year,
-      from,
-      to,
-    }: {
-      month: number;
-      year: number;
-      from: string;
-      to: string;
-    }) => {
-      const [start, end] = getMonthBoundary(month, year);
+  test.each<[number, number, string, string]>([
+    [9 - 1, 2023, '2023-08-27', '2023-09-30'],
+    [8 - 1, 2023, '2023-07-30', '2023-09-02'],
+    [10 - 1, 2023, '2023-10-01', '2023-11-04'],
+  ])('calculates boundary dates for %i + 1/%i', (month, year, from, to) => {
+    const [start, end] = getMonthBoundary(month, year);
 
-      expect(start).toBe(from);
-      expect(end).toBe(to);
-    },
-  );
+    expect(start).toBe(from);
+    expect(end).toBe(to);
+  });
 });
