800c6c1ccf5f4bc4da72ba2a90ebc8fe8546665d
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2023-10-06 00:10:31 +0200

    Disable preflight (#140)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,7 @@ module.exports = {
   darkMode: ['class', '[data-mantine-color-scheme="dark"]'],
   content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
   plugins: [require('tailwindcss-animate')],
+  corePlugins: {
+    preflight: false,
+  },
 };
