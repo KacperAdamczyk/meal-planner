46adac4f96f6374302dc645b0b26baa66109fdbb
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2023-10-07 21:39:05 +0200

    Move tailwind base to new layer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,4 @@ module.exports = {
   darkMode: ['class', '[data-mantine-color-scheme="dark"]'],
   content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
   plugins: [require('tailwindcss-animate')],
-  corePlugins: {
-    preflight: false,
-  },
 };
