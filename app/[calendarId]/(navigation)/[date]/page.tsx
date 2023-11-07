47eaff9f6aaf30fbc9e6a0044f3aa2be40a9195a
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2023-11-07 23:57:24 +0100

    Calendar placeholder

--- app/[calendarId]/(navigation)/[date]/page.tsx
+++ app/[calendarId]/(navigation)/[date]/page.tsx
@@ -5,28 +5,39 @@ import { IconSquareRoundedPlus } from '@tabler/icons-react';
 import { MealGroupPlaceholder } from '@/components/custom/MealGroup/MealGroupPlaceholder';
 import { CalendarPlaceholder } from '@/components/custom/CalendarPlaceholder';
 import { LinkButton } from '@/components/composite/LinkButton';
+import { parseISO } from 'date-fns';
 
 const Date: FC<{
   params: { calendarId: string; date: string };
   searchParams: { month?: string };
-}> = ({ params: { calendarId, date }, searchParams: { month } }) => (
-  <div className="grid grid-cols-[max-content_1fr] grid-rows-[max-content_1fr] gap-2">
-    <div className="row-span-2">
-      <Suspense fallback={<CalendarPlaceholder />}>
-        <DashboardCalendar calendarId={calendarId} date={date} month={month} />
-      </Suspense>
+}> = ({ params: { calendarId, date }, searchParams: { month } }) => {
+  const placeholderDate = parseISO(date);
+
+  return (
+    <div className="grid grid-cols-[max-content_1fr] grid-rows-[max-content_1fr] gap-2">
+      <div className="row-span-2">
+        <Suspense
+          fallback={<CalendarPlaceholder placeholderDate={placeholderDate} />}
+        >
+          <DashboardCalendar
+            calendarId={calendarId}
+            date={date}
+            month={month}
+          />
+        </Suspense>
+      </div>
+      <div className="col-start-2 justify-self-end">
+        <LinkButton href={`/${calendarId}/${date}/new`} action>
+          <IconSquareRoundedPlus />
+        </LinkButton>
+      </div>
+      <div className="col-start-2 row-start-2 flex flex-col items-stretch">
+        <Suspense fallback={<MealGroupPlaceholder rows={3} />}>
+          <SelectedDay calendarId={calendarId} date={date} />
+        </Suspense>
+      </div>
     </div>
-    <div className="col-start-2 justify-self-end">
-      <LinkButton href={`/${calendarId}/${date}/new`} action>
-        <IconSquareRoundedPlus />
-      </LinkButton>
-    </div>
-    <div className="col-start-2 row-start-2 flex flex-col items-stretch">
-      <Suspense fallback={<MealGroupPlaceholder rows={3} />}>
-        <SelectedDay calendarId={calendarId} date={date} />
-      </Suspense>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Date;
