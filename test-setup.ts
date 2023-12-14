7c02a82bf949c00ae5870d8f432c0b6d33f37054
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2023-12-14 12:30:59 +0100

    Testcontainers

--- test-setup.ts
+++ test-setup.ts
@@ -3,8 +3,17 @@ import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { afterAll, beforeAll } from 'bun:test';
 import { seed } from '@/db/seed-data';
 import { sql } from 'drizzle-orm';
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
+
+let container: StartedPostgreSqlContainer | undefined;
 
 beforeAll(async () => {
+  container = await new PostgreSqlContainer().withDatabase('test-db').start();
+  process.env.DATABASE_URL = container.getConnectionUri();
+
   console.log('Preparing test database...');
   await db.execute(sql`CREATE SCHEMA IF NOT EXISTS auth`);
   console.log('Migrating test database...');
@@ -15,4 +24,5 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await pool.end();
+  await container?.stop();
 });
